9d60cfcb975c758e1b47333fe3f8778e8205c893
Aman Sinha <amansinha@Amans-MacBook-Air.local>
2026-05-16 10:35:12 +0530

    first commit

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int a = 5;
+        if(a == 6){
+            ;
+        } else {
+            System.out.println("Java");
+        }
+
+        if(a * 2 > 5 || a / 0 > 1)
+            System.out.println("Safe");
+        
+        // class TernaryOperator{
+
+        //    what is ternary operator?
+        //     A temporary operator is a replacement of the if-else block, basically a short form of if else is covered within the line
+        // }
+
+        //  Why its called ternary operator?
+        //  It is called ternary becuase, its uses three operands.
+    }
+}
