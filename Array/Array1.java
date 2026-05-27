1a924922432c441f4fd6e2de799443238703c080
AmannSinhaaa <amansinha798498@gmail.com>
2026-05-27 09:03:46 +0530

    added

--- Array/Array1.java
+++ Array/Array1.java
@@ -9,7 +9,9 @@ public class Array1 {
             arr[i] = sc.nextInt();
         }
         for(int i = 0; i < n; i++){
+            if(arr[i] % 2 == 0){
             System.out.print(arr[i] + " ");
+            }
         }
     }
 }
