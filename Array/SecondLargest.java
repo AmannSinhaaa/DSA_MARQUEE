3250b7b7408cd1252edc4852db71e0de931f4ef1
AmannSinhaaa <amansinha798498@gmail.com>
2026-05-29 08:12:56 +0530

    changed

--- Array/SecondLargest.java
+++ Array/SecondLargest.java
@@ -1,27 +1,29 @@
 package Array;
 import java.util.*;
 public class SecondLargest {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i = 0; i < n ; i++){
-            arr[i] = sc.nextInt();
-        }
-
+    public static void SecondLargest(int[] arr, int n) {
         int largest = arr[0];
-        for(int i = 0; i < n; i++){
-            if(arr[i] > largest){
-                largest = arr[i];
-            }
-        }
-
         int second_largest = -1;
         for(int i = 0; i < n; i++){
-            if(arr[i] > second_largest && largest != arr[i]){
+            if(arr[i] > largest){
+                second_largest = largest;
+                largest = arr[i];
+         } else if(arr[i] > second_largest && largest != arr[i]){
                 second_largest = arr[i];
-            }
+        }
         }
         System.out.print(second_largest);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        SecondLargest(arr, n);
     }
 }
